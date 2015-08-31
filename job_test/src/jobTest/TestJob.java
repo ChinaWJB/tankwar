8c607dd65917beed1779f9eaff69ee4dd55342fe
ChinaWJB <chinawjb@foxmail.com>
2015-08-31 22:33:59 +0800

    tankwar

--- job_test/src/jobTest/TestJob.java
+++
@@ -1,7 +0,0 @@
-package jobTest;
-
-public class TestJob {
-	public static void main(String[] args) {
-		javax.swing.JOptionPane.showMessageDialog(null,"注册中心开始监听！");
-	}
-}
--- job_test/src/jobTest/Test_RunTime.java
+++
@@ -1,9 +0,0 @@
-package jobTest;
-
-import java.io.File;
-import java.io.IOException;
-public class Test_RunTime {
-	public static void main(String[] args) throws IOException{
-		Runtime.getRuntime().exec("cmd /k start D:\\Tomcat2\\apache-tomcat-6.0.43\\bin\\startup.bat");
-	}
-}
